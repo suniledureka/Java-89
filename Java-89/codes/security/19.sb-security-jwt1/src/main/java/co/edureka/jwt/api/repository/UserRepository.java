11edaea18b0e80e154d3d7c1f290a27abb204e35
Sunil Joseph <sunil.edureka@gmail.com>
2022-07-08 14:09:42 +0530

    java

--- Java-89/codes/security/19.sb-security-jwt1/src/main/java/co/edureka/jwt/api/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package co.edureka.jwt.api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import co.edureka.jwt.api.entity.User;
-
-public interface UserRepository extends JpaRepository<User,Integer> {
-    User findByUserName(String username);
-}
