30cb0bd34a5dc8034963a863bed772e0fd6b4f2b
sunilozhakal <sunilozhakal@gmail.com>
2022-07-30 22:59:39 +0530

    java

--- Java-89/codes/spring/ajax/src/main/java/co/edureka/ems/dao/EmployeeDao.java
+++
@@ -1,7 +0,0 @@
-package co.edureka.ems.dao;
-
-import co.edureka.ems.entity.Employee;
-
-public interface EmployeeDao {
-	public Employee findEmployeeById(Integer eno);
-}
--- Java-89/codes/spring/ajax/src/main/java/co/edureka/ems/services/EmployeeService.java
+++
@@ -1,7 +0,0 @@
-package co.edureka.ems.services;
-
-import co.edureka.ems.entity.Employee;
-
-public interface EmployeeService {
-	public Employee searchEmployeeById(Integer eno);
-}
